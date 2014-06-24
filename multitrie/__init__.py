3d65f2252ca79ec8ef26b2c078e9ce5526daae9b
Michael Wu <michaelmwu@gmail.com>
2014-06-24 15:33:33 -0700

    api import

--- multitrie/__init__.py
+++ multitrie/__init__.py
@@ -3,4 +3,4 @@ __version__ = '0.0.1'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Pixelapse'
 
-from .api import check_call, check_output, call_command, call_output
+from .api import MultiTrie
